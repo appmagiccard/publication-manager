12c07b8712c68ac643bc357163016e025f5323c9
zf <san.ferreiro92@gmail.com>
2024-01-30 16:42:21 -0300

    fix double table in manyToMany relationship

--- src/main/java/com/magicauction/publicationmanager/entity/Publication.java
+++ src/main/java/com/magicauction/publicationmanager/entity/Publication.java
@@ -25,7 +25,7 @@ public class Publication {
     @JoinColumn(name="publisher_id", referencedColumnName = "userId")
     private User publisher;
 
-    @ManyToMany
+    @ManyToMany(mappedBy = "publications")
     private Set<Offer> offers;
 
     @ManyToOne
